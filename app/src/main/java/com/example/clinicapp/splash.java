fc376c5e5d958a157e24e53b9ff770250954cbd9
sarielpinto <48193176+sarielpinto@users.noreply.github.com>
2019-06-04 20:03:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/clinicapp/splash.java
@@ -0,0 +1,23 @@
+package com.example.clinicapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+    private final int DURACION_SPLASH = 3000; // 3 segundos
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(splash.this, Menu.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
