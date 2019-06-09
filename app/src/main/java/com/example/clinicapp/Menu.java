f2ff9207193d8fe2d5bb13395c85067041350401
LuisYama <yamaluis3@gmail.com>
2019-06-08 21:59:46 -0500

    especialistas

--- app/src/main/java/com/example/clinicapp/Menu.java
+++ app/src/main/java/com/example/clinicapp/Menu.java
@@ -1,8 +1,10 @@
 package com.example.clinicapp;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Explode;
 import android.view.View;
 
 public class Menu extends AppCompatActivity {
@@ -14,9 +16,10 @@ public class Menu extends AppCompatActivity {
     }
 
     public void citas(View view) {
-        Intent intent = new Intent(getApplicationContext(), Citas.class);
+        Intent intent= new Intent(Menu.this,Main2Activity.class);
         startActivity(intent);
     }
+
 
     public void Especilistas(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
---
+++ app/src/main/java/com/example/clinicapp/especialistas/Contact2.java
@@ -0,0 +1,18 @@
+package com.example.clinicapp.especialistas;
+
+public class Contact2 {
+    String especialidad;
+    String logo;
+
+    public Contact2() {
+    }
+
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+}
