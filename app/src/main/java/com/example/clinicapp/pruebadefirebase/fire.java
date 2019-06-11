bd381d7642958e5430231faaa4862173b9931b92
sarielpinto <48193176+sarielpinto@users.noreply.github.com>
2019-06-11 06:05:00 -0500

    vamos_con toquio

--- app/src/main/java/com/example/clinicapp/pruebadefirebase/fire.java
+++ app/src/main/java/com/example/clinicapp/pruebadefirebase/fire.java
@@ -1,5 +1,7 @@
 package com.example.clinicapp.pruebadefirebase;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -20,48 +23,51 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class fire extends AppCompatActivity {
-    public static  final String TAG="FIREBASE";
-    TextView te_1,te_2;
-    private  int dia,mes,ano,hora,minutos;
+    public static final String TAG = "FIREBASE";
+    TextView te_1, te_2;
+    private int dia, mes, ano, hora, minutos;
     String month;
     String day;
     String mas;
 
     public RecyclerView recyclerListView;
-    public  UserAdapter myAdapter;
+    public UserAdapter myAdapter;
     public EditText editTextName;
     public EditText editTextCountry;
-    public  EditText editTextWeight;
-    public  static TextView textViewEmptyView;
+    public EditText editTextWeight;
+    public static TextView textViewEmptyView;
     Button buttonAdd;
     public ProgressBar myProgressBar;
     DatabaseReference databaseReference;
-    String e,n,es;
+    String e, n, es;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire);
-        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         // creating layout
         creatingLayouts();
-        e=getIntent().getStringExtra("Hola");
-        es=getIntent().getStringExtra("especialista");
-        n=getIntent().getStringExtra("nombre");
+        e = getIntent().getStringExtra("Hola");
+        es = getIntent().getStringExtra("especialista");
+        n = getIntent().getStringExtra("nombre");
 
     }
-    public void creatingLayouts(){
-        myProgressBar=(ProgressBar) findViewById(R.id.loader);
+
+    public void creatingLayouts() {
+        myProgressBar = (ProgressBar) findViewById(R.id.loader);
         textViewEmptyView = (TextView) findViewById(R.id.tvEmptyView);
         editTextName = (EditText) findViewById(R.id.nameEditText);
-        editTextCountry=(EditText) findViewById(R.id.countryEditText);
-        editTextWeight=(EditText) findViewById(R.id.weightEditText);
+        editTextCountry = (EditText) findViewById(R.id.countryEditText);
+        editTextWeight = (EditText) findViewById(R.id.weightEditText);
         buttonAdd = (Button) findViewById(R.id.addButton);
-        recyclerListView=(RecyclerView) findViewById(R.id.recylerview_list);
+        recyclerListView = (RecyclerView) findViewById(R.id.recylerview_list);
         recyclerListView.setLayoutManager(new LinearLayoutManager(this));
-        myAdapter= new UserAdapter(this);
+        myAdapter = new UserAdapter(this);
         updateAdapter();
         recyclerListView.setAdapter(myAdapter);
     }
@@ -70,9 +76,9 @@ public class fire extends AppCompatActivity {
     public void btnAddOnClick(View v) {
 
         String name = editTextName.getText().toString().trim();
-        String country=editTextCountry.getText().toString().trim();
-        double weight=Double.parseDouble(editTextWeight.getText().toString().trim());
-        User user= new User(name, country, weight);
+        String country = editTextCountry.getText().toString().trim();
+        double weight = Double.parseDouble(editTextWeight.getText().toString().trim());
+        User user = new User(name, country, weight);
 
         if (TextUtils.isEmpty(name)) {
             Toast.makeText(getApplicationContext(), "Please enter name",
@@ -91,7 +97,7 @@ public class fire extends AppCompatActivity {
     }
 
     // adding new user to end  the user using on firebase database
-    public void updateDatabase(User user){
+    public void updateDatabase(User user) {
 
         databaseReference.child("users").push().setValue(user);
 
@@ -104,9 +110,9 @@ public class fire extends AppCompatActivity {
     }
 
     //update adapter
-    public void updateAdapter(){
+    public void updateAdapter() {
 
-        final List<User> listUsers= new ArrayList<>();
+        final List<User> listUsers = new ArrayList<>();
         databaseReference.child("users").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
@@ -131,7 +137,7 @@ public class fire extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(getApplicationContext(),"Canceled",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -140,7 +146,7 @@ public class fire extends AppCompatActivity {
 
 
     //display the user on Adapter
-    public void displayUsers(List<User> ls){
+    public void displayUsers(List<User> ls) {
         myProgressBar.setVisibility(View.GONE);
         textViewEmptyView.setVisibility(View.GONE);
         recyclerListView.setVisibility(View.VISIBLE);
@@ -150,11 +156,45 @@ public class fire extends AppCompatActivity {
         myAdapter.setData(ls);
         myAdapter.notifyDataSetChanged();
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
 
+    public void onClick(View view) {
+        Intent miIntent = null;
+        switch (view.getId()) {
+            case R.id.b_1:
+                final Calendar c = Calendar.getInstance();
+                dia = c.get(Calendar.DAY_OF_MONTH);
+                mes = c.get(Calendar.MONTH);
+                ano = c.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        //te_1.setText(year+"/"+"0"+(monthOfYear+1)+"/"+dayOfMonth);
+                        if (monthOfYear < 10) {
+
+                            month = "0" + monthOfYear;
+                        } else if (monthOfYear >= 10) {
+                            month = "" + monthOfYear;
+                        }
+                        if (dayOfMonth < 10) {
+
+                            day = "0" + dayOfMonth;
+                        } else if (dayOfMonth >= 10) {
+                            day = "" + dayOfMonth;
+                        }
+                        editTextWeight.setText(year  + month +  day);
+                    }
+                }
+                        , ano, mes, dia);
+                datePickerDialog.show();
+                break;
+
+
+        }
+    }
 }
-
-
